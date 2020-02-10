4afe74d6c4d5977c9d0726555575c899bd7c93e5
Wai Leung (Kent) <47157699+wai0901@users.noreply.github.com>
2020-02-09 19:04:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.post("/", (req, res) => {
         url:"https://us20.api.mailchimp.com/3.0/lists/3861c7c23f",
         method: "POST",
         headers: {
-            "Authorization": "wai 46b6f5c2b2ceb928fb8977218e965ca2-us20"
+            "Authorization": "wai xxx"
         },
         body: jasonData
     };
@@ -65,8 +65,3 @@ app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 });
 
-// api key
-// ec841f5861c130c41a8839a6b5e428f0-us20
-
-// audience id
-// 3861c7c23f
